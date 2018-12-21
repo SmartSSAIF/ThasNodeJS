7c6bb76e4894db6c8ef20c33227c81691dbd6aef
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-12-21 19:19:29 -0200

    Pedidos

--- app/controller/pedido.js
+++ app/controller/pedido.js
@@ -4,12 +4,6 @@ module.exports.get = function (app, req, res) {
     var genericDAO = new app.app.models.GenericDAO(connection);
 
     var query = "call produtos()";
-
-
-    const {
-        Expo
-    } = require('expo-server-sdk');
-    let expo = new Expo();
     genericDAO.execute(query, function (error, result) {
         if (error) {
             console.log(error);
@@ -36,19 +30,19 @@ module.exports.post = function (app, req, res) {
     genericDAO.create(requisicao, "pedido", function (error, result) {
         if (!error) {
             var query = "SELECT * FROM pedidoproduto ORDER BY ID DESC LIMIT 1";
-           genericDAO.execute(query, (error, result) => {
-                for(let i in pedidos){  
+            genericDAO.execute(query, (error, result) => {
+                for (let i in pedidos) {
                     console.log("i ", i);
                     console.log(result);
                     console.log('Result [0] ', result[0]);
                     var insert = "INSERT INTO pedidoproduto (idproduto, idpedido) values (?,?);"
-                    genericDAO.execute(insert, [i.id, result[0].id], (error, result)=> {
-                        
+                    genericDAO.execute(insert, [i.id, result[0].id], (error, result) => {
+
                     });
                 }
                 res.status(200).send("sei la");
-           });
-          
+            });
+
         } else {
             console.log(error);
             res.status(500).send(error);
