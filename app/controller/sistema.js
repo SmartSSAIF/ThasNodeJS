8b7fcc5af1d3c0924d614c4533524925f67e7e67
gabrielbaldao <gabrielbaldao@gmail.com>
2019-02-08 16:37:36 -0200

    sistema, basico de procurar carro, pedido e enviar para o carro certo

--- app/controller/sistema.js
+++ app/controller/sistema.js
@@ -1,20 +1,19 @@
 var zerorpc = require("zerorpc");
 var client = new zerorpc.Client();
+var http = require("http");
+
 client.connect("tcp://127.0.0.1:5005");
+
 module.exports.get = function (app, req, res) {
 
 
 
 
 }
-const procuraCaminho = function (car, menor,origem, destino) {
-    console.log("before promise")
+const procuraCaminho = function (car, menor, origem, destino) {
     return new Promise(function (resolve, reject) {
-        console.log('dentro da promise', car);
         client.invoke("pedido", [origem, destino, car], function (error, res, more) {
-            console.log('distancia \t', res.length);
             if (res.length < menor.distancia) {
-                console.log("dentro log ", car.id);
 
                 menor.carro = car.id;
                 menor.distancia = res.length;
@@ -28,111 +27,154 @@ const procuraCaminho = function (car, menor,origem, destino) {
         });
     });
 }
-const buscaPedido = function(idPedido, genericDAO){
-    console.log(idPedido, "\t buscar")
-    return new Promise(function(resolve, reject){
-        
-        genericDAO.find({idPedido: idPedido}, 'pedidoproduto', function(err, res){
-            if(err){
+const buscaPedido = function (idPedido, genericDAO) {
+    return new Promise(function (resolve, reject) {
+
+        genericDAO.find({ idPedido: idPedido }, 'pedidoproduto', function (err, res) {
+            if (err) {
                 console.log(err);
                 reject({});
-            }else{
-                
-                console.log('encontrou ', res);
+            } else {
                 resolve(res);
             }
         });
     })
+}
+const loopFor = function (menor, vetor) {
+    return new Promise(function (resolve, reject) {
+
+        for (var i = 0; i < vetor.length; i++) {
+            var car = vetor[i];
+            let x = async function procura(car, menor) {
+                const valor = await procuraCaminho(car, menor, 'peteca' + car.id, 'peteca11');
+
+
+            };
+            x(car, menor, 'peteca' + car.id, 'peteca11');
+        }
+        console.log('fim for ', menor);
+        if (menor.distancia < 999) {
+
+            resolve(menor);
+        }
+        else {
+            reject({ carro: -1, distancia: -999 });
+        }
+
+    });
+}
+const jsonConcat = function (o1, o2) {
+    for (var key in o2) {
+        o1[key] = o2[key];
+    }
+    return o1;
 }
 module.exports.post = function (app, req, res) {
 
 
     var requisicao = req.body;
     var connection = app.config.dbConnection();
-
     var genericDAO = new app.app.models.GenericDAO(connection);
-    console.log('sistema rolando');
-    /*
-    Procurar os pedidos
-    Procurar os carros
-    Enviar as instruções para eles
-    
-    
-    */
 
-    /*
-        Procura pedidos
-    */
-   //Ordenar por data
-   genericDAO.execute('select * from pedido where statusPedido = 1 order by prioridade desc', function(err, resultado){
+    genericDAO.execute('select * from pedido where statusPedido = 1 order by prioridade desc', function (err, resultado) {
+        for (var j in resultado) {
+            let pedido = resultado[j].id;
+            let x = async function procuraPedido(pedido, genericDAO) {
+                const valor = await buscaPedido(pedido, genericDAO);
+                return valor;
+            };
+            var carro;
+            let y = x(pedido, genericDAO).then(valor => {
+                console.log('after promise ', valor)
+                genericDAO.find({ estado: true }, 'carros', function (err, resp) {
+                    if (err) {
+                        console.log(err);
+                        console.log("error");
+                        return res.status(400).send({ erro: 1 });
+                    } else {
+                        if (valor.length > 0 && resp.length > 0) {
+                            var output = jsonConcat(resp[0], valor[0])
+                            console.log('saida ', output);
 
-    console.log(resultado);
-    console.log('\n\n\n\n');
-    for(var j in resultado){
-        let pedido = resultado[j].id;
-        let x = async function procuraPedido(pedido, genericDAO) {
-            console.log("chegou");
-            const valor = await buscaPedido(pedido, genericDAO);
-            console.log('promise final ', valor);
-        };
-        x(pedido, genericDAO);
-       console.log("for");
-    }
-   });
-    genericDAO.find({ estado: true }, 'carros', function (err, result) {
-        console.log("buscou");
-        if (err) {
-            console.log(err);
-            console.log("error");
-            return res.status(400).send({ erro: 1 });
-        } else {
-            console.log('carros disponiveis')
-            console.log(result);
-            // res.status(200).send(result);
-            //Fazer logica para selecionar o melhor carro
-            // let carroMenorDistancia = 5000;
-            // for(let i=0; i< result.length; i++){
-            //Chamar o rmi pedido, da posicao atual do carro com a origem do pedido(na verdade, fazer outro metodo para verificar a distancia no python), assim descobrir a menor distancia
-            // }
-            var menor = {
-                carro: 0,
-                distancia: 999,
-            }
+                            /*
+                                Enviar comando para o carrinho, 
+                            */
+                           var options = {
+                            hostname: '127.0.0.1',
+                            port: 3000,
+                            path: '/teste',
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            }
+                        };
+                        var reqCarro = http.request(options, function (res) {
+                            // console.log('Status: ' + res.statusCode);
+                            // console.log('Headers: ' + JSON.stringify(res.headers));
+                            res.setEncoding('utf8');
+                            res.on('data', function (body) {
+                                console.log('Body: ' + body);
+                            });
+                        });
 
-            for (var i = 0; i < result.length; i++) {
-                var car = result[i];
-                console.log("car\n", car);
-               let x = async function procura(car,menor) {
-                    const valor = await procuraCaminho(car, menor,'peteca'+car.id,'peteca11');
-                    console.log('promise ', valor);
-                };
-                x(car,menor,'peteca'+car.id,'peteca11');
+                        reqCarro.write(JSON.stringify(output));
+                        reqCarro.end();
+                           // return res.status(200).send(output);
+                        }
 
-            }
+                        // console.log(result);
+                        // res.status(200).send(result);
+                        //Fazer logica para selecionar o melhor carro
+                        // let carroMenorDistancia = 5000;
+                        // for(let i=0; i< result.length; i++){
+                        //Chamar o rmi pedido, da posicao atual do carro com a origem do pedido(na verdade, fazer outro metodo para verificar a distancia no python), assim descobrir a menor distancia
+                        // }
+                        //     var menor = {
+                        //         carro: 0,
+                        //         distancia: 999,
+                        //     }
+                        //     let resFor = async function loop(menor, result){
+                        //         const buscaCarro = await loopFor(menor, result);
+                        //         // console.log('fim promise ', buscaCarro );
+                        //     }
+                        //     resFor(menor, result);
+                        // }
+
+                        /*
+                            Aqui possui o melhor carro para ocasiao
+                        */
+                        // console.log(menor);
+                        //return res.status(200).send(resp[0]);
+
+                    }
+
+                });
 
 
-            /*
-                Aqui possui o melhor carro para ocasiao
-            */
-            console.log(menor);
-            return res.status(200).send(menor);
+
+            });
+
         }
     });
-    console.log("fim");
-  //  connection.end();
-    /*
+
+}
+
+
+
+
+//  connection.end();
+/*
 Ao fim da invocação, 
 *Selecionar o melhor veiculo
 *Enviar as instruções para o veiculo
 * -Logica para acompanhar o veiculo
 */
 
-    //-------------------------------------------------------------------
+//-------------------------------------------------------------------
 
 
 
-    //connection.end();
-}
+//connection.end();
 
 
 module.exports.delete = function (app, req, res) {
@@ -143,8 +185,3 @@ module.exports.put = function (app, req, res) {
 
 }
 
-module.exports.testePost = function (app, req, res) {
-    console.log('Teste');
-    console.log(req.body);
-    res.status(200).send(req.body);
-}
--- app/controller/teste.js
+++ app/controller/teste.js
@@ -1,3 +1,8 @@
 module.exports.get = function (app, req, res) {
     res.status(200).send({deubom: 1});
 }
+module.exports.post = function (app, req, res) {
+    console.log('Teste');
+    console.log(req.body);
+    res.status(200).send(req.body);
+}
